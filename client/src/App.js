284eaceb5a8c4e2964eb1bd763ff50b5d8b54a1a
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-05-10 15:23:06 +0300

    Router-Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Home } from "./pages";
+import { Home, ProductDetails } from "./pages";
 import { Header, Footer } from "./layout";
 import "./sass-helpers/globals.scss";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+      </Routes>
       <Footer />
     </>
   );
