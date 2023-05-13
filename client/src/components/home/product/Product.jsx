df1a3fb6151ac09948bc91f2147c0ac9e172f0a8
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-05-13 23:35:03 +0300

    Add product component

---
+++ client/src/components/home/product/Product.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { Button, Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "../rating/Rating";
+
+const Product = ({
+  id: productId,
+  name,
+  company,
+  img,
+  description,
+  rating,
+  numReviews,
+}) => {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} className="gy-4">
+      <Card className={styles.card}>
+        <Link to={`/products/${productId}`}>
+          <Card.Img src={img} alt={name} />
+        </Link>
+        <Card.Body className={styles["card-body"]}>
+          <Card.Title>{name}</Card.Title>
+          <Card.Subtitle>{company}</Card.Subtitle>
+          <Card.Text>{description}</Card.Text>
+          <Rating rating={rating} numReviews={numReviews} />
+          <Button
+            type="button"
+            variant="primary"
+            className={styles["card-btn"]}
+          >
+            add to cart
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Product;
---
+++ client/src/components/home/rating/Rating.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+
+const Rating = ({ rating, numReviews }) => {
+  return (
+    <div className={styles.rating}>
+      <span>
+        {rating >= 1 ? (
+          <FontAwesomeIcon icon={faStar} />
+        ) : rating >= 0.5 ? (
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+        ) : (
+          <FontAwesomeIcon icon={emptyStar} />
+        )}
+      </span>
+
+      <span>
+        {rating >= 2 ? (
+          <FontAwesomeIcon icon={faStar} />
+        ) : rating >= 1.5 ? (
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+        ) : (
+          <FontAwesomeIcon icon={emptyStar} />
+        )}
+      </span>
+
+      <span>
+        {rating >= 3 ? (
+          <FontAwesomeIcon icon={faStar} />
+        ) : rating >= 2.5 ? (
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+        ) : (
+          <FontAwesomeIcon icon={emptyStar} />
+        )}
+      </span>
+
+      <span>
+        {rating >= 4 ? (
+          <FontAwesomeIcon icon={faStar} />
+        ) : rating >= 3.5 ? (
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+        ) : (
+          <FontAwesomeIcon icon={emptyStar} />
+        )}
+      </span>
+
+      <span>
+        {rating >= 5 ? (
+          <FontAwesomeIcon icon={faStar} />
+        ) : rating >= 4.5 ? (
+          <FontAwesomeIcon icon={faStarHalfStroke} />
+        ) : (
+          <FontAwesomeIcon icon={emptyStar} />
+        )}
+      </span>
+
+      <span className={styles.num}>{numReviews} Reviews</span>
+    </div>
+  );
+};
+
+export default Rating;
