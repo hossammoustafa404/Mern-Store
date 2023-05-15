cd0ad3d2572d3253caf75552270d988020dfab58
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-05-15 11:00:26 +0300

    Refactor home code

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import styles from "./styles.module.scss";
 import { Content } from "../../layout";
 import { Link } from "react-router-dom";
@@ -6,27 +6,69 @@ import axios from "axios";
 import { Product } from "../../components";
 import { Container, Row } from "react-bootstrap";
 
+const initialState = {
+  products: [],
+  error: "",
+  loading: true,
+};
+
+const PRODUCTS_FETCHED = "PRODUCTS_FETCHED";
+const FETCH_ERROR = "FETCH_ERROR";
+
+const productsReducer = (state, action) => {
+  switch (action.type) {
+    case PRODUCTS_FETCHED:
+      return { ...state, products: action.payload, loading: false };
+
+    case FETCH_ERROR:
+      return { ...state, error: action.payload, loading: false };
+
+    default:
+      return state;
+  }
+};
+
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+  const [{ products, error, loading }, dispatch] = useReducer(
+    productsReducer,
+    initialState
+  );
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios("./dummyData.json");
-      setProducts(data.products);
+      try {
+        const { data } = await axios("/api/v1/products");
+        dispatch({ type: PRODUCTS_FETCHED, payload: data.products });
+      } catch (error) {
+        dispatch({ type: FETCH_ERROR, payload: error });
+      }
     };
 
     fetchProducts();
   }, []);
 
-  const renderedProducts = products.map((product) => (
-    <Product key={product.id} {...product} />
-  ));
+  let renderedProducts = [];
+  if (products.length) {
+    renderedProducts = products.map((product) => (
+      <Product key={product.id} {...product} />
+    ));
+  }
+
   return (
     <Content className={styles.home}>
       <section>
         <Container>
           <h2 className={styles.title}>featured</h2>
-          <Row>{renderedProducts}</Row>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : renderedProducts.length ? (
+            <Row>{renderedProducts}</Row>
+          ) : (
+            <p>No products to show</p>
+          )}
         </Container>
       </section>
     </Content>
--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,6 +1,6 @@
 // Get all products
 const getAllProducts = async (req, res) => {
-  res.send("get all products");
+  res.json({ products: [] });
 };
 
 module.exports = { getAllProducts };
