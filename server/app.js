ced9fb172bcb4cb2b841713818dc3ae501a4e973
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-05-15 10:19:19 +0300

    Connect server to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // Start Requires
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connect");
 // End Requires
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 // The start of the app
 const port = process.env.PORT || 5000;
 const start = async () => {
+  await connectDB(process.env.MONGO_URI);
+  console.log("Connected to database...");
   app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 };
 
